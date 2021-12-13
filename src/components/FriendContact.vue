5a7d4fe42a2ed1026c0f672721e709177b22a368
somnathsardar <githubsomnath1996@gmail.com>
2021-12-13 15:14:20 +0530

    Created components and passing props to components

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <h3>{{ name }}</h3>
+    <button v-on:click="toggleSection()">Show details</button>
+    <ul v-if="showDetails">
+      <li><strong>Phone: </strong>{{ phone }}</li>
+      <li><strong>Email: </strong>{{ email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["name", "phone", "email"],
+  data: function() {
+    return {
+      showDetails: false,
+    }
+  },
+  methods: {
+    toggleSection() {
+      this.showDetails = !this.showDetails;
+    }
+  }
+}
+</script>
